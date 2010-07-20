52b9889a9e0b99ce2eae71e47692a161743ba218
philippe.laflamme <philippe.laflamme@d3583f0e-5265-309b-6f84-1721cadfc23d>
2010-07-20 14:16:43 +0000

    [maven-release-plugin]  copy for tag r40

---
+++ tags/r40/plugin/src/main/java/gwtquery/plugins/ui/UiWidget.java
@@ -0,0 +1,132 @@
+package gwtquery.plugins.ui;
+
+import com.google.gwt.core.client.JavaScriptObject;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
+import com.google.gwt.query.client.Function;
+
+/**
+ * An abstract base class for all JQuery UI widgets.
+ * 
+ * @param <T> the widget type. Used for returning the proper type for method chaining
+ * @param <O> the widget's options class
+ * 
+ * @author Philippe Laflamme
+ */
+public abstract class UiWidget<T extends UiWidget<?, ?>, O extends WidgetOptions<?>> extends Ui {
+
+  protected final String widgetType;
+
+  protected JavaScriptObject ui = null;
+
+  /**
+   * Constructor for building a jQuery-UI widget.
+   * 
+   * @param nodeList the current selection
+   * @param widgetType the type of widget to build (button, accordion, etc.)
+   * @param options the widget options (may be null).
+   */
+  protected UiWidget(NodeList<Element> nodeList, String widgetType, O options) {
+    super(nodeList);
+    this.widgetType = widgetType;
+    ui = initUiWidget(widgetType, nodeList, options);
+  }
+
+  public T destroy() {
+    invoke("destroy");
+    return getWidgetInstance();
+  }
+
+  public T disable() {
+    invoke("disable");
+    return getWidgetInstance();
+  }
+
+  public T enable() {
+    invoke("enable");
+    return getWidgetInstance();
+  }
+
+  public T option(String options) {
+    invoke("option", options);
+    return getWidgetInstance();
+  }
+
+  public T option(O options) {
+    invoke("option", options);
+    return getWidgetInstance();
+  }
+
+  public T option(String options, Object value) {
+    invoke("option", options, value);
+    return getWidgetInstance();
+  }
+
+  /**
+   * Binds a custom event to a callback function. This is used by jQuery-ui's event binding system.
+   * 
+   * @param name the widget's custom event name
+   * @param f the callback function. The instance should override {@code Function#f(com.google.gwt.user.client.Event,
+   * Object)}
+   * @return this
+   */
+  public native final T bind(String name, Function f) /*-{
+    this.@gwtquery.plugins.ui.UiWidget::ui = this.@gwtquery.plugins.ui.UiWidget::ui.bind(name, function(event, ui) {
+      f.@com.google.gwt.query.client.Function::f(Lcom/google/gwt/user/client/Event;Ljava/lang/Object;)(event, ui);
+    });
+    return this;
+  }-*/;
+
+  protected void invoke(String method) {
+    ui = invoke(ui, widgetType, method);
+  }
+
+  protected void invoke(String method, Object arg) {
+    ui = invoke(ui, widgetType, method, arg);
+  }
+
+  protected void invoke(String method, int arg) {
+    ui = invoke(ui, widgetType, method, arg);
+  }
+
+  protected void invoke(String method, boolean arg) {
+    ui = invoke(ui, widgetType, method, arg);
+  }
+
+  protected void invoke(String method, String option, Object value) {
+    invoke(ui, widgetType, method, option, value);
+  }
+
+  protected native final T getWidgetInstance() /*-{
+    return this;
+  }-*/;
+
+  protected native final int invokeForInt(JavaScriptObject ui, String type, String method) /*-{
+    return ui[type](method);
+  }-*/;
+
+  private native final JavaScriptObject invoke(JavaScriptObject ui, String type, String method) /*-{
+    return ui[type](method);
+  }-*/;
+
+  private native final JavaScriptObject invoke(JavaScriptObject ui, String type, String method, Object arg) /*-{
+    return ui[type](method, arg);
+  }-*/;
+
+  private native final JavaScriptObject invoke(JavaScriptObject ui, String type, String method, Object arg1, Object arg2) /*-{
+    return ui[type](method, arg1, arg2);
+  }-*/;
+
+  private native final JavaScriptObject invoke(JavaScriptObject ui, String type, String method, int arg) /*-{
+    return ui[type](method, arg);
+  }-*/;
+
+  private native final JavaScriptObject invoke(JavaScriptObject ui, String type, String method, boolean arg) /*-{
+    return ui[type](method, arg);
+  }-*/;
+
+  private native final JavaScriptObject initUiWidget(String type, NodeList<Element> list, O options) /*-{
+    return $wnd.jQuery(list)[type](options);
+  }-*/;
+
+}
---
+++ tags/r40/plugin/src/main/java/gwtquery/plugins/ui/widgets/Menu.java
@@ -0,0 +1,57 @@
+package gwtquery.plugins.ui.widgets;
+
+import gwtquery.plugins.ui.Ui;
+import gwtquery.plugins.ui.UiPlugin;
+import gwtquery.plugins.ui.UiWidget;
+import gwtquery.plugins.ui.WidgetOptions;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
+
+/**
+ * Implements jQuery-UI's Menu
+ * 
+ * @see <a href="http://docs.jquery.com/UI/Menu">jQuery-UI's Menu</a>
+ * 
+ * @author Philippe Laflamme
+ */
+public class Menu extends UiWidget<Menu, Menu.Options> {
+
+  public static class Options extends WidgetOptions<Options> {
+    
+    protected Options() {
+      
+    }
+
+    public static native final Options create() /*-{
+      return {};
+    }-*/;
+  }
+ 
+  /**
+   * Used to register the plugin.
+   */
+  private static class MenuPlugin implements UiPlugin<Menu> {
+
+    public Menu init(Ui ui, WidgetOptions<?> options) {
+      return new Menu(ui.get(), (Menu.Options)options.cast());
+    }
+
+  }
+
+  public static final Class< Menu> Menu = Menu.class;
+
+  static {
+    registerPlugin(Menu.class, new MenuPlugin());
+  }
+
+  public Menu(NodeList<Element> list, Menu.Options options) {
+    super(list, "menu", options);
+  }
+  
+  public Menu refresh() {
+    invoke("refresh");
+    return this;
+  }
+
+}
