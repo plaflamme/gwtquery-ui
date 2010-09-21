d13659e16402b44c646aedd1efff130d05236b73
philippe.laflamme <philippe.laflamme@d3583f0e-5265-309b-6f84-1721cadfc23d>
2010-09-21 17:54:59 +0000

    [maven-release-plugin]  copy for tag r104

---
+++ tags/r104/plugin/src/main/java/gwtquery/plugins/ui/widgets/Buttonset.java
@@ -0,0 +1,45 @@
+package gwtquery.plugins.ui.widgets;
+
+import gwtquery.plugins.ui.Ui;
+import gwtquery.plugins.ui.UiPlugin;
+import gwtquery.plugins.ui.UiWidget;
+import gwtquery.plugins.ui.WidgetOptions;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
+
+/**
+ * Implements jQuery-UI's Buttonset
+ * 
+ * @see <a href="http://docs.jquery.com/UI/Button">jQuery-UI's Buttonset</a>
+ * 
+ * @author Philippe Laflamme
+ */
+public class Buttonset extends UiWidget<Buttonset, Button.Options> {
+
+  /**
+   * Used to register the plugin.
+   */
+  private static class ButtonPlugin implements UiPlugin<Buttonset> {
+
+    public Buttonset init(Ui ui, WidgetOptions<?> options) {
+      return new Buttonset(ui.get(), (Button.Options) options.cast());
+    }
+  }
+
+  public static final Class<Buttonset> Buttonset = Buttonset.class;
+
+  static {
+    registerPlugin(Buttonset.class, new ButtonPlugin());
+  }
+
+  public Buttonset(NodeList<Element> list, Button.Options options) {
+    super(list, "buttonset", options);
+  }
+
+  public Buttonset refresh() {
+    invoke("refresh");
+    return this;
+  }
+
+}
